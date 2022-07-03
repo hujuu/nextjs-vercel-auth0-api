9bf63be4aaf6575c0d75f2f8d3f77b96f4769774
hujuu <out.of.the.base.sfc@gmail.com>
2022-07-03 22:33:09 +0900

    Update products.js

--- pages/api/products.js
+++ pages/api/products.js
@@ -6,7 +6,7 @@ export default withApiAuthRequired(async function products(req, res) {
   const { accessToken } = await getAccessToken(req, res, {
     scopes: ["read:messages"],
   });
-  const response = await fetch(process.env.API_ENDPOINT, {
+  const response = await fetch(`${process.env.API_ENDPOINT}/api/private`, {
     headers: {
       Authorization: `Bearer ${accessToken}`,
     },
