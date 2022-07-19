75d386ad46fc80292d7f87149da5ad57d49d733e
hujuu <out.of.the.base.sfc@gmail.com>
2022-07-19 21:47:56 +0900

    Update show.js

--- pages/api/show.js
+++ pages/api/show.js
@@ -1,8 +1,6 @@
 import { getAccessToken, withApiAuthRequired } from "@auth0/nextjs-auth0";
 
 export default withApiAuthRequired(async function products(req, res) {
-  // If your Access Token is expired and you have a Refresh Token
-  // `getAccessToken` will fetch you a new one using the `refresh_token` grant
   const { accessToken } = await getAccessToken(req, res, {
     scopes: ["read:messages"],
   });
